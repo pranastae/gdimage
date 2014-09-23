d03bcd2ca356f20a0de259a5ef11c9eea1729213
Jose Luis Quintana <joseluisquintana20gmail.com>
2014-09-23 14:43:49 -0500

    Update rotate.php example

--- examples/rotate.php
+++ examples/rotate.php
@@ -3,8 +3,8 @@
 require '../src/GDImage/GDUtils.php';
 require '../src/GDImage/GDImage.php';
 
-// Rotate an image with a given angle
+// Rotate an image to 90º
 $image = new GDImage();
-$image->load('img.jpg');
-$image->rotate(180);
+$image->load('http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=100.jpg');
+$image->rotate(90);
 $image->output();
